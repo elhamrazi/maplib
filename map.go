686bb29124a253ba92c65c5e05520dac71f0cc31
Elham Razi <47900695+elhamrazi@users.noreply.github.com>
2023-06-11 15:13:21 +0330

    Update map.go

--- map.go
+++ map.go
@@ -5,3 +5,8 @@ type Location struct {
 	Latitude  float64 `json:"latitude"`
 	Longitude float64 `json:"longitude"`
 }
+
+func PrintLocation (l Location) {
+	fmt.Println("this is lat: ", l.Latitude)
+	fmt.Println("this is lon: ", l.Longitude)
+}
